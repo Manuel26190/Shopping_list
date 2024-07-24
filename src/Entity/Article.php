e67dd85e12438ceb92fbeb82bb812c6f756bd513
Manuel26190 <manuelfaure26190@gmail.com>
2024-07-24 16:37:58 +0200

    save post valid

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,33 +4,38 @@ namespace App\Entity;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
-class Article{
-    private ?int $id;
+class Article
+{
+    private ?int $id = null;  // ID est optionnel et initialisé à null
 
-    #[Assert\NotBlank]
+    #[Assert\NotBlank]  // Content ne peut pas être vide
     private string $content;
 
-    
-    public function __construct(int $id, string $content){ 
+    public function __construct(?int $id = null, string $content)  // ID est optionnel
+    { 
         $this->id = $id;
         $this->content = $content;
     }
 
-    
-
-    public function getId(): ?int{
+    public function getId(): ?int
+    {
         return $this->id;
     }
-    public function setId(int $id){
+
+    public function setId(?int $id): self
+    {
         $this->id = $id;
-        return $this;
-    }  
-    public function getContent(): string{
-        return $this->content;
-    }
-    public function setContent(string $content): self{
-        $this->content = $content;
         return $this;
     }
 
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+        return $this;
+    }
 }
